20f875007eb5ac98b708dc5d15d95791c52987bd
jasmeet <jasmeet2109@outlook.com>
2018-07-30 21:50:38 +0530

    file_added

---
+++ app/src/main/java/com/example/kingj/movieholic/CreditsPojo.java
@@ -0,0 +1,42 @@
+
+package com.example.kingj.movieholic;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+
+@SuppressWarnings("unused")
+public class CreditsPojo {
+
+    @Expose
+    private List<Cast> cast;
+    @Expose
+    private List<Crew> crew;
+    @Expose
+    private Long id;
+
+    public List<Cast> getCast() {
+        return cast;
+    }
+
+    public void setCast(List<Cast> cast) {
+        this.cast = cast;
+    }
+
+    public List<Crew> getCrew() {
+        return crew;
+    }
+
+    public void setCrew(List<Crew> crew) {
+        this.crew = crew;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
---
+++ app/src/main/java/com/example/kingj/movieholic/Dates.java
@@ -0,0 +1,32 @@
+
+package com.example.kingj.movieholic;
+
+//import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+
+//@Generated("net.hexar.json2pojo")
+@SuppressWarnings("unused")
+public class Dates {
+
+    @Expose
+    private String maximum;
+    @Expose
+    private String minimum;
+
+    public String getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(String maximum) {
+        this.maximum = maximum;
+    }
+
+    public String getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(String minimum) {
+        this.minimum = minimum;
+    }
+
+}
